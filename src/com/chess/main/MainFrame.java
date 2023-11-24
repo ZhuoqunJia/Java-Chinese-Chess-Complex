568f894685b23f8a08f2ae76f59a5bdacb1de82c
ZhuoqunJia <1347618115@qq.com>
2023-11-24 09:28:57 +0800

    Swing按钮点击事件

--- src/com/chess/main/MainFrame.java
+++ src/com/chess/main/MainFrame.java
@@ -11,45 +11,79 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainFrame implements ActionListener {
-    public static void main(String[] args) {
-        //创建了有一个名称为frame的JFrame实例对象，实例又称对象
-        JFrame frame = new JFrame();
-        //创建实例的格式：类名 实例名 = new 类的构造方法();
-        //创建实例的格式：类名 实例名 = new 类名();
-        //JFrame默认是一个看不见的窗口
+public class MainFrame extends JFrame implements ActionListener {
+
+    public MainFrame(){
         //设置窗口的大小
-        frame.setSize(550,500);
+        this.setSize(550,500);
         //设置窗口居中
-        frame.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null);
         //设置点击关闭按钮同时结束JVM，每一个Java运行的程序都是一个JVM，是一个JVM进程
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //设置布局管理器
-        frame.setLayout(new BorderLayout());
+        this.setLayout(new BorderLayout());
         //将游戏面板添加到窗口中
-        frame.add(new GamePanel(), BorderLayout.CENTER);
+        this.add(new GamePanel(), BorderLayout.CENTER);
 //        System.out.println("==================");
         //添加按钮面板
         JPanel btnPanel = new JPanel(new GridLayout(4, 1));
-        frame.add(btnPanel, BorderLayout.EAST);
+        this.add(btnPanel, BorderLayout.EAST);
         JLabel hintLabel = new JLabel("红方走");
         btnPanel.add(hintLabel);
+
         JButton btnHuiQi = new JButton("悔棋");
+        btnHuiQi.setActionCommand("huiqi");
+        btnHuiQi.addActionListener(MainFrame.this);
         btnPanel.add(btnHuiQi);
+
         JButton btnSave = new JButton("保存棋谱");
+        btnSave.setActionCommand("baocun");
+        btnSave.addActionListener(this);
         btnPanel.add(btnSave);
+
         JButton btnImport = new JButton("导入棋谱");
+        btnImport.setActionCommand("daoru");
+        btnImport.addActionListener(this);
         btnPanel.add(btnImport);
+
         JButton btnQiuHe = new JButton("求和");
+        btnQiuHe.setActionCommand("qiuhe");
+        btnQiuHe.addActionListener(this);
         btnPanel.add(btnQiuHe);
+
         JButton btnRenShu = new JButton("认输");
+        btnRenShu.setActionCommand("renshu");
+        btnRenShu.addActionListener(this);
         btnPanel.add(btnRenShu);
+
         //设置窗口可见，建议放在后面
-        frame.setVisible(true);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        //执行构造方法
+        MainFrame mainFrame = new MainFrame();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        System.out.println("按钮被点击了");
+        String cmd = e.getActionCommand();
+        switch (cmd){
+            case "huiqi":
+                System.out.println("huiqi");
+                break;
+            case "baocun":
+                System.out.println("baocun");
+                break;
+            case "daoru":
+                System.out.println("daoru");
+                break;
+            case "qiuhe":
+                System.out.println("qiuhe");
+                break;
+            case "renshu":
+                System.out.println("renshu");
+                break;
+        }
     }
 }
