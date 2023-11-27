fce8b2541c8f08531345497f6c275ccc3af1564c
ZhuoqunJia <1347618115@qq.com>
2023-11-27 17:46:42 +0800

    网络编程基础

--- src/com/chess/main/MainFrame.java
+++ src/com/chess/main/MainFrame.java
@@ -18,9 +18,9 @@ public class MainFrame extends JFrame implements ActionListener {
 
     private final GamePanel gamePanel;
 
-    public MainFrame(){
+    public MainFrame() {
         //设置窗口的大小
-        this.setSize(555,500);
+        this.setSize(555, 500);
         //设置窗口居中
         this.setLocationRelativeTo(null);
         //设置点击关闭按钮同时结束JVM，每一个Java运行的程序都是一个JVM，是一个JVM进程
@@ -66,6 +66,7 @@ public class MainFrame extends JFrame implements ActionListener {
         //设置窗口可见，建议放在后面
         this.setVisible(true);
     }
+
     public static void main(String[] args) {
         //执行构造方法
         MainFrame mainFrame = new MainFrame();
@@ -75,7 +76,7 @@ public class MainFrame extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         System.out.println("按钮被点击了");
         String cmd = e.getActionCommand();
-        switch (cmd){
+        switch (cmd) {
             case "huiqi":
                 System.out.println("huiqi");
                 this.gamePanel.huiqi();
@@ -106,57 +107,61 @@ public class MainFrame extends JFrame implements ActionListener {
         int result = chooser.showOpenDialog(null);
         File parent = chooser.getSelectedFile();
 
-        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(parent))){
-            Object object = ois.readObject();
-            System.out.println(object);
-            if(object instanceof Chess[]){
-                gamePanel.setChesses((Chess[]) object);
+        if (null != parent) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(parent))) {
+                Object object = ois.readObject();
+                System.out.println("获取的Chess数组对象为===" + object);
+                if (object instanceof Chess[]) {
+                    gamePanel.setChesses((Chess[]) object);
+                }
+            } catch (FileNotFoundException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            } catch (ClassNotFoundException classNotFoundException) {
+                classNotFoundException.printStackTrace();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
             }
-        }catch (FileNotFoundException fileNotFoundException){
-            fileNotFoundException.printStackTrace();
-        }catch (ClassNotFoundException classNotFoundException){
-            classNotFoundException.printStackTrace();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
         }
     }
 
     /**
      * 保存棋谱
      */
-    private void save(){
+    private void save() {
         JFileChooser chooser = new JFileChooser();
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         int result = chooser.showOpenDialog(null);
         File parent = chooser.getSelectedFile();
         System.out.println("parent-->" + parent);
-        //创建文件
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss-SSS");
-        String path = parent.getAbsolutePath() + File.separator + simpleDateFormat.format(new Date(System.currentTimeMillis())) + ".txt";
-        File file = new File(path);
-        if(!file.exists()){
-            try {
-                file.createNewFile();
-            } catch (IOException ex) {
-                ex.printStackTrace();
+        if (null != parent) {
+            //创建文件
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss-SSS");
+            String path = parent.getAbsolutePath() + File.separator + simpleDateFormat.format(new Date(System.currentTimeMillis())) + ".txt";
+            File file = new File(path);
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
-        }
-        //创建对象字节输出流
-        ObjectOutputStream oos = null;
-        try{
-            //处理流和节点流
-            oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(gamePanel.getChesses());
-        }catch (FileNotFoundException e1){
-            e1.printStackTrace();
-        }catch(IOException ioException){
-            ioException.printStackTrace();
-        }finally {
-            if(null != oos){
-                try{
-                    oos.close();
-                }catch (IOException ioException){
-                    ioException.printStackTrace();
+            //创建对象字节输出流
+            ObjectOutputStream oos = null;
+            try {
+                //处理流和节点流
+                oos = new ObjectOutputStream(new FileOutputStream(file));
+                oos.writeObject(gamePanel.getChesses());
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } finally {
+                if (null != oos) {
+                    try {
+                        oos.close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
                 }
             }
         }
